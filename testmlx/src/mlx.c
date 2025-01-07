c59d92a45ddd0ce41647d1a20328034771b13ab2
Raneem Abusalah <rabusala@c2r3s7.42amman.com>
2025-01-07 17:42:56 +0300

    a

---
+++ testmlx/src/mlx.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nagha <nagha@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 16:21:18 by nagha             #+#    #+#             */
+/*   Updated: 2025/01/05 17:23:15 by nagha            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int main(void)
+{
+    t_data data;
+    data.mlx = mlx_init();
+   
+    data.window = mlx_new_window(data.mlx, 800, 600, "MiniLibX Test Window");
+    
+    mlx_key_hook(data.window, handle_esc, &data);
+    mlx_hook(data.window, 17, 0, handle_cross, &data);
+    mlx_loop(data.mlx);
+
+    return 0;
+}
